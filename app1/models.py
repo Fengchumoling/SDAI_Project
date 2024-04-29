58e111e9ce35ddc700a238db8b7bdd1f373b3b0c
Wind <20213802007@m.scnu.edu.cn>
2024-04-29 16:21:27 +0800

    模型迁移Project，Activity

--- app1/models.py
+++ app1/models.py
@@ -12,8 +12,9 @@ class User(models.Model):
     def __str__(self):
         return self.name
 
+
 class Project(models.Model):
-    project_name = models.CharField(max_length=20)
+    project_name = models.CharField(max_length=50)
     project_activity = models.ForeignKey('Activity', on_delete=models.CASCADE)
 
     def __str__(self):
@@ -25,7 +26,7 @@ class Activity(models.Model):
     activity_start_date = models.DateField()
     activity_end_date = models.DateField()
     activity_duration = models.DurationField()
-    activity_resource = models.ManyToManyField('User', on_delete=models.CASCADE)
+    activity_resource = models.ManyToManyField('User')
 
-
-
+    def __str__(self):
+        return self.activity_name
