c819749b9cfdf448a5d62baf3800d801c4358d9d
Zhuoer-Zhang <u10zz21@abdn.ac.uk>
2024-04-29 16:10:24 +0800

    建立了project和activity的模型

--- app1/models.py
+++ app1/models.py
@@ -12,4 +12,18 @@ class User(models.Model):
     def __str__(self):
         return self.name
 
+class Project(models.Model):
+    project_name = models.CharField(max_length=20)
+    project_activity = models.ForeignKey('Activity', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.project_name
+
+
+class Activity(models.Model):
+    activity_name = models.CharField(max_length=20)
+    activity_start_date = models.DateField()
+    activity_end_date = models.DateField()
+    activity_duration = models.DurationField()
+    activity_resource = models.ManyToManyField('User', on_delete=models.CASCADE)
 
