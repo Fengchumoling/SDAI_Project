67bd1d48edec698706d4cf9f5570d9b19af4e4d9
Wind <20213802007@m.scnu.edu.cn>
2024-04-30 15:58:12 +0800

    主页导航栏，等待实现甘特图

--- app1/admin.py
+++ app1/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
-from app1.models import User
+from app1.models import User, Project, Activity
+
 # Register your models here.
 
 admin.site.register(User)
+
+admin.site.register(Project)
+
+admin.site.register(Activity)
--- app1/views.py
+++ app1/views.py
@@ -6,5 +6,6 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'index.html')
 
+
 def d3j(request):
     return render(request, 'd3j.html')
