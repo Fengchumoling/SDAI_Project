53ffd78388157b3e54002e02af8bc0b1aad28770
Zhuoer-Zhang <u10zz21@abdn.ac.uk>
2024-05-25 14:49:30 +0800

    甘特图灯箱改动（加入了进度设置并且加入了滚动条）以及一些注释

--- app1/models.py
+++ app1/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 # Create your models here.
 
+#Create a user model, store user information for login, chat room use, and permission control
 class User(models.Model):
     name = models.CharField(max_length=20)
     email = models.EmailField()
@@ -13,6 +14,7 @@ class User(models.Model):
         return self.name
 
 
+#The model that creates the project stores the Task and name of the project
 class Project(models.Model):
     project_name = models.CharField(max_length=50)
     project_activity = models.ForeignKey('Activity', on_delete=models.CASCADE)
@@ -21,6 +23,7 @@ class Project(models.Model):
         return self.project_name
 
 
+#The model that creates the Task(Activity) stores the information about each Tasks
 class Activity(models.Model):
     activity_name = models.CharField(max_length=20)
     activity_start_date = models.DateField()
@@ -33,6 +36,7 @@ class Activity(models.Model):
         return self.activity_name
 
 
+#The model is used to store the content and logic of the WBS diagram
 class WBSElement(models.Model):
     name = models.CharField(max_length=255)
     parent = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE)
